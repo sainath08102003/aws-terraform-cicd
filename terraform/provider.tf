0d71e38c958d5c5ad44a84548a3bcc58f5198886
sainath <dhagesainath88@gmail.com>
2026-07-12 09:18:15 +0000

    just added s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,8 +6,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "sai-terraform-state-08102003"
+    key            = "aws-terraform-cicd/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
+
